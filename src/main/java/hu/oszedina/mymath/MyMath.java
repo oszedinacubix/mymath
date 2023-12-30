75d42288eecf8996e9f519af000f0b9950cd8db9
Edina Osz <oszedina.cubix@gmail.com>
2023-12-30 13:24:08 +0100

    MATH-4 Implement divide

--- src/main/java/hu/oszedina/mymath/MyMath.java
+++ src/main/java/hu/oszedina/mymath/MyMath.java
@@ -12,10 +12,15 @@ public class MyMath {
     public static int myMultiply(int a, int b) {
         return a * b;
     }
+    
+    public static double myDivide(int a, int b) {
+        return 1.0 * a / b;
+    }
 
     public static void main(String[] args) {
         System.out.println(myAdd(3, 2));
         System.out.println(mySubtract(5, 2));
         System.out.println(myMultiply(3, 2));
+        System.out.println(myDivide(3, 2));
     }
 }
