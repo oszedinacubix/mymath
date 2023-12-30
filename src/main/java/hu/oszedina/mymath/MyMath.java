acb9ddff4b2c33bf0213e58bc28a9bd45e3de04d
Edina Osz <oszedina.cubix@gmail.com>
2023-12-30 12:51:51 +0100

    MATH-1: Implement add

--- src/main/java/hu/oszedina/mymath/MyMath.java
+++ src/main/java/hu/oszedina/mymath/MyMath.java
@@ -1,5 +1,11 @@
 package hu.oszedina.mymath;
 
 public class MyMath {
+    public static int myAdd(int a, int b) {
+        return a + b;
+    }
 
+    public static void main(String[] args) {
+        System.out.println(myAdd(3, 2));
+    }
 }
