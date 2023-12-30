c7f1f6746a09ce70a6d62a4f10d6e165f60d3bdd
Edina Osz <oszedina.cubix@gmail.com>
2023-12-30 12:58:14 +0100

    MATH-2 Implement subtract

--- src/main/java/hu/oszedina/mymath/MyMath.java
+++ src/main/java/hu/oszedina/mymath/MyMath.java
@@ -4,8 +4,13 @@ public class MyMath {
     public static int myAdd(int a, int b) {
         return a + b;
     }
+    
+    public static int mySubtract(int a, int b) {
+        return a - b;
+    }
 
     public static void main(String[] args) {
         System.out.println(myAdd(3, 2));
+        System.out.println(mySubtract(5, 2));
     }
 }
